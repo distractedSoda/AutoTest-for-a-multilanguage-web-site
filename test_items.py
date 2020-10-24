fb301b5c5d4c785083b654da01daf82c0af1ea0b
Val Ilicheva <73257941+distractedSoda@users.noreply.github.com>
2020-10-24 06:51:59 +0300

    add time.sleep(30) as a comment

--- test_items.py
+++ test_items.py
@@ -1,8 +1,11 @@
+#import time
+
 link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
 
 
 def test_search_basket_button(browser):
     browser.get("http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/")
+    #test.sleep(30)
     button = browser.find_element_by_css_selector("#add_to_basket_form")
     assert button is not None, "It's not a basket button =("
     print("Found a basket button")
